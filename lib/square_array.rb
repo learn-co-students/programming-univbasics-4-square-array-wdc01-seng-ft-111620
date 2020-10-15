b4c79bd747e41fc91dbce61c0ab7e0776d42eb1a
Arifur Rahman <arifrah125@gmail.com>
2020-10-15 19:56:35 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,13 @@
+
+#take an array and square each value and return in a new array 
 def square_array(array)
-  # your code here
+  new_array = []
+  
+  array.length.times { |i|
+    new_array[i] = array[i] **2  
+  }
+  
+  new_array
 end
+
+#puts square_array([1,2,3])
